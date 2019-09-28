a0a75b5bb46e337de0312401c0e4a05e9734c4d2
rafael <rafael__silva15@outlook.com>
2019-09-28 17:42:16 -0300

    Adição dos players

--- Blackjack.py
+++ Blackjack.py
@@ -1,10 +1,20 @@
-cartas = [
+N_players= int(input("Digite o número de jogadores (Máximo quatro): "))
+Players=[]
+numero = 1
+while numero <= N_players:
+    player=input("Player {0} digite seu nome: ".format(numero))
+    Players.append(player)
+    numero+=1
+print(Players)
+
+cartas = {
         '2','3','4','5','6','7','8','9','10','Q','J','K',
         '2','3','4','5','6','7','8','9','10','Q','J','K',
         '2','3','4','5','6','7','8','9','10','Q','J','K',
         '2','3','4','5','6','7','8','9','10','Q','J','K'
-        ]
+        }
+
 Carteira = float(input("Quandos você tem em dinheiro? R$:"))
 
-while carteira>=20:
+
     
