f0ebc7b7d7184e623d3a4b468b6f94462cff854d
unknown <barbaramd@al.insper.edu.br>
2019-09-30 17:34:46 -0300

    coloquei carteira

--- Blackjack.py
+++ Blackjack.py
@@ -1,10 +1,36 @@
-cartas = [
-        '2','3','4','5','6','7','8','9','10','Q','J','K',
-        '2','3','4','5','6','7','8','9','10','Q','J','K',
-        '2','3','4','5','6','7','8','9','10','Q','J','K',
-        '2','3','4','5','6','7','8','9','10','Q','J','K'
-        ]
-Carteira = float(input("Quandos você tem em dinheiro? R$:"))
+import random
 
-while carteira>=20:
-    
+APOSTAMINIMA = float(5)
+
+
+def players ():
+    N_players= int(input("Digite o número de jogadores (Máximo quatro): "))
+    Players=[]
+    numero = 1
+    while numero <= N_players:
+        player=input("Player {0} digite seu nome: ".format(numero))
+        Players.append(player)
+        numero+=1
+    return Players
+
+def ValorDinheiro (listaplayers,ApostaMinima):
+    Player_Dinheiro = {}
+    i=0
+    while i < len(listaplayers):
+        carteira = float(input("Quanto você tem em dinheiro {}? R$:".format(listaplayers[i])))
+        if carteira < ApostaMinima :
+            while carteira < ApostaMinima:
+                carteira = float(input("Valor menor que a aposta. Digite quanto você tem em dinheiro {}? R$:".format(listaplayers[i])))
+        Player_Dinheiro[listaplayers[i]] = carteira
+        i +=1
+    return Player_Dinheiro
+
+baralho = {
+        '2':2, '3':3,'4':4,'5':5,'6':6,'7':7,'8':8, '9':9, '10':10,'Q': 10,'J': 10, 'K': 10, 'ÁS': 11,
+        '2':2, '3':3,'4':4,'5':5,'6':6,'7':7,'8':8, '9':9, '10':10,'Q': 10,'J': 10, 'K': 10, 'ÁS': 11,
+        '2':2, '3':3,'4':4,'5':5,'6':6,'7':7,'8':8, '9':9, '10':10,'Q': 10,'J': 10, 'K': 10, 'ÁS': 11,
+        '2':2, '3':3,'4':4,'5':5,'6':6,'7':7,'8':8, '9':9, '10':10,'Q': 10,'J': 10, 'K': 10, 'ÁS': 11
+        }
+
+ListaPlayers = players()
+print (ValorDinheiro(ListaPlayers,APOSTAMINIMA))
